adecc243aa6b7210e61ce03c274f18be3a5b388c
Jake Matthews <matthewsj17@winthrop.edu>
2026-03-23 22:10:29 -0400

    DONE

--- Assigment4.1/assignment04_lists.cpp
+++ Assigment4.1/assignment04_lists.cpp
@@ -1,16 +1,33 @@
 /*************************************************************************
 * Programming Assignment 4 for CSCI 271-001 Spring 2026
 *
-* Author: William Klement (REPLACE THIS WITH YOUR OWN NAME)
-* OS: Ubuntu Debian Linux 21.1
+* Author: Jake Matthews 
+* OS: Windows 11
 * Compiler: g++
-* Date: March 9, 2026
+* Date: March 23, 2026
 *
 * Purpose
 * This program implements the class Lists as linked lists of generic nodes 
 * The task is to implement the missing methods. 
 * Please see corresponding assignemnt questions
 *************************************************************************/
+
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* Jake Matthews 
+********************************************************************/
 
 #include<iostream>
 #include <ctime>
