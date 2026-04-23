25560873339a6d6583ccd6139ac43b29de3244f7
Jake Matthews <matthewsj17@winthrop.edu>
2026-04-23 13:09:37 -0400

    Added docu.

--- Assignment6/assignment6.java
+++ Assignment6/assignment6.java
@@ -1,3 +1,36 @@
+/*************************************************************************
+* Project 6 for CSCI 271-001 Spring 2026
+*
+* Author: Jake Matthews
+* OS: Windows 11
+* Compiler: javac 21.0.9
+* Date: April 23rd, 2026
+*
+* Purpose
+* This program reads a list of scores from an input file, counts how many
+* scores there are in the list, and computes their sum and average.
+*
+*************************************************************************/
+
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* Jake Matthews
+********************************************************************/
+
+
+
 // Java Program to Implement a Generic Binary Tree
 
 // Importing all input output classes
@@ -47,40 +80,114 @@ class BinaryTree<T extends Comparable<T>> {
 // no way sweet it works. -Jake 1:43pm 4/21/26
 
 // ADD THING
+
+/*****************************sizeRecursively(Node<T> root)>****************************
+ * Description: searches through the tree, and calculates the size of the tree. (# of nodes)
+ *
+ * Parameters: Node<T> root, the 'root' of the tree structure
+ *
+ * Pre: a tree must be created, and the root supplied to the function.
+ *
+ * Post: the tree's size will be calculated recursively and returned to the user.
+ *
+ * Returns: an int that is the size of the tree data structure.
+ * 
+ * Called by: size(), sizeRecursively()
+ * Calls: sizeRecursively()
+ ************************************************************************/
   private int sizeRecursively(Node<T> root){
-    int size = 0;
-    if(root!=null){
-      size = sizeRecursively(root.left) + sizeRecursively(root.right) + 1;
+    int size = 0; // initiallize a basic size value as 0
+    if(root!=null){ // if the root isn't null, IE the tree exists, 
+      size = sizeRecursively(root.left) + sizeRecursively(root.right) + 1; // we add one to the size of both children.
     }
-    return size;
+    return size; // if we hit the bottom, we return the size back!
   }
 
-  public T getMax(){
-    return getMaxRecursively(this.root);
+
+  /*****************************getMax()****************************
+ * Description: searches through the tree, and finds the maximum value present in it
+ *
+ * Parameters: N/A
+ *
+ * Pre: a tree must be created.
+ *
+ * Post: the tree's maximum value will be returned to the user
+ *
+ * Returns: a type T that is the maximum value in the tree data structure.
+ * 
+ * Called by: n/a
+ * Calls: getMaxRecursively()
+ ************************************************************************/
+  public T getMax(){ // the 'public interface' of the recursive function
+    return getMaxRecursively(this.root); // call the private function that can access the node class stuff.
    
   }
 
+
+/*****************************getMaxRecursively(Node<T> root)>****************************
+ * Description: searches through the tree, and finds the maximum value present in it
+ *
+ * Parameters: Node<T> root, the 'root' of the tree structure
+ *
+ * Pre: a tree must be created, and the root supplied to the function.
+ *
+ * Post: the tree's maximum value found recursively and returned to the user.
+ *
+ * Returns: a type T that is the maximum value in the tree data structure.
+ * 
+ * Called by: getMax(), getMaxRecursively()
+ * Calls: getMaxRecursively()
+ ************************************************************************/
   private T getMaxRecursively(Node<T> root){
-    if(root.right == null){
-      T max = root.element;
-      return max;
+    // since the highest value is stored in the bottom right of the tree, we just increment to it!
+    if(root.right == null){  // there's nothing to the right, we're at the highest value
+      return root.element; // assign it to the Max, and return it.
     }
-    else{
+    else{ // if there is an element to the right, we go to it recursively!
       return getMaxRecursively(root.right);
     }
   }
 
+   /*****************************getMin()****************************
+ * Description: searches through the tree, and finds the minimum value present in it
+ *
+ * Parameters: N/A
+ *
+ * Pre: a tree must be created.
+ *
+ * Post: the tree's minimum value will be returned to the user
+ *
+ * Returns: a type T that is the minimum value in the tree data structure.
+ * 
+ * Called by: n/a
+ * Calls: getMinRecursivelyJake()
+ ************************************************************************/
   public T getMin(){
     return getMinRecursivelyJake(this.root);
    
   }
 
-  private T getMinRecursivelyJake(Node<T> root){
-    if(root.left == null){
-      T min = root.element;
-      return min;
+  /*****************************getMinRecursivelyJake()****************************
+ * Description: searches through the tree, and finds the minimum value present in it
+ *
+ * Parameters: N/A
+ *
+ * Pre: a tree must be created.
+ *
+ * Post: the tree's minimum value will be returned to the user
+ *
+ * Returns: a type T that is the minimum value in the tree data structure.
+ * 
+ * Called by: getMin()
+ * Calls: getMinRecursivelyJake()
+ ************************************************************************/
+// named because getMinRecursively was already defined below, 
+// not sure if i needed to create my own or not so i did!
+  private T getMinRecursivelyJake(Node<T> root){ 
+    if(root.left == null){ // if there is no left child to the root, then we are at the lowest value!
+      return root.element; // return it to the user!
     }
-    else{
+    else{ // otherwise, we move to the child on the left and compare again!
       return getMinRecursivelyJake(root.left);
     }
   }
@@ -164,16 +271,29 @@ class BinaryTree<T extends Comparable<T>> {
     return findRecursively(root, item);
   }
 
-
+ /*****************************findRecursively()****************************
+ * Description: searches through the tree, and attempts to find a value 
+ * the user supplies
+ * 
+ * Parameters: Node<T> root, and T item
+ *
+ * Pre: a tree must be created, and a valid item must be supplied to find
+ *
+ * Post: a boolean of if the value is in the tree will be returned to the user
+ *
+ * Returns: a boolean, true if the item is in the tree, false if otherwise.
+ * 
+ * Called by: find()
+ * Calls: findRecursively()
+ ************************************************************************/
   private boolean findRecursively(Node<T> root, T item){
 
-    if(root == null)
+    if(root == null) // if the root is null, then we didn't find the item in the tree.
       return false;
-    if(root.element == item){
+    else if(root.element == item){ // 
       return true;
     }
     else{
-
       int compareVal = item.compareTo(root.element);
       if(compareVal < 0){
         return findRecursively(root.left, item);
@@ -182,7 +302,7 @@ class BinaryTree<T extends Comparable<T>> {
         return findRecursively(root.right, item);
       }
       else{ // if its not the same, and not any smaller or larger, than its just not there.
-        return false;
+        return false; // may be redundant?
       }
 
 
